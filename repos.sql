5c4eaa1b4123a2038204535b800979a5b116c0c9
c1aris <goolhanrry@gmail.com>
2020-03-07 20:11:19 +0800

    Add the sql file for constructing the table

---
+++ repos.sql
@@ -0,0 +1,16 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for repos
+-- ----------------------------
+DROP TABLE IF EXISTS `repos`;
+CREATE TABLE `repos` (
+  `repo` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `state` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastRequestIp` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastRequestTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`repo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
